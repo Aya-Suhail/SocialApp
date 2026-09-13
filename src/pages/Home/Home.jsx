7cf47b9e010ed3f30b0cd943c21b9952194f0c9a
Aya-Suhail <ayaalbasuny62@gmail.com>
2026-09-13 14:50:43 +0300

    Import Helmet for document head management
    Added Helmet import for managing document head.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import AllPosts from "../../components/pages/AllPosts/AllPosts";
 import CreatePost from "../../components/shared/craetePost/CreatePost";
+import { Helmet } from "react-helmet-async";
 
 
 const Home = () => {
